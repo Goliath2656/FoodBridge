8bbf81462b3b1c64e3b1bb5fbd27ba7616a85fb3
Abdul Wasil <161149797+Goliath2656@users.noreply.github.com>
2026-02-07 18:52:44 +0530

    update7

--- donations/models.py
+++ donations/models.py
@@ -5,8 +5,13 @@ class Donation(models.Model):
     donor = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='donation_app_donations'  
+        related_name='donations'
     )
     food_name = models.CharField(max_length=100)
     quantity = models.PositiveIntegerField()
+    pickup_location = models.CharField(max_length=255)  
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.food_name
 
