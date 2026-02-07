676f08e3a5e6abe30d4efe6ace59d5ff662976cf
Abdul Wasil <161149797+Goliath2656@users.noreply.github.com>
2026-02-07 18:50:27 +0530

    update6

--- donations/models.py
+++ donations/models.py
@@ -2,9 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Donation(models.Model):
-    donor = models.ForeignKey(User, on_delete=models.CASCADE)
+    donor = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='donation_app_donations'  
+    )
     food_name = models.CharField(max_length=100)
     quantity = models.PositiveIntegerField()
-    pickup_location = models.TextField()
-    is_available = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+
